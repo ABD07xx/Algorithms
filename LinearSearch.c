71cc0ebb6395da4acbafb825efd7152d3d7d575a
ABDULLAH FAZILI <66874041+ABD07xx@users.noreply.github.com>
2021-12-25 18:42:47 +0530

    Add files via upload

---
+++ LinearSearch.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+	int i,temp,size,a[100],element;
+	printf("Enter size of array: ");
+	scanf("%d",&size);
+	for(i=0;i<size;i++){
+		printf("Enter element %d: ",i+1);
+		scanf("%d",&a[i]);
+	}
+		printf("Array Formed:\n");
+	for(i=0;i<size;i++){
+	printf("%d\t",a[i]);
+	}
+	printf("\nEnter the Element You Want to search: ");
+	scanf("%d",&element);
+	for(i=0;i<size;i++){
+		if(a[i]==element){
+			printf("\nLocation is %d: ",i+1);
+		}
+		
+	}
+
+}
